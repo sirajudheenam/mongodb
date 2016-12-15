696246ee696d64c867a810be082aff867ecd4239
Sirajudheen Mohamed Ali <sirajudheen.mohamed.ali@sap.com>
2016-12-15 14:18:31 +0100

    some changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['mongodb']['version'] = '3.4'
 
 default['mongodb']['config_file'] = '/etc/mongod.conf'
 default['mongodb']['data_path'] = '/var/lib/mongo'
-default['mongodb']['pid']['path'] = '/var/run/mongodb'
+default['mongodb']['pid']['path'] = "/var/run/mongodb"
 default['mongodb']['pid'] = '/var/run/mongodb/mongod.pid'
 default['mongodb']['log_path'] = '/var/log/mongodb/mongod.log'
 default['mongodb']['port'] = '27017'
