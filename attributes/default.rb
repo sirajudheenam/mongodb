8b1a28e22a78368edf70c5c70149d881b30a0c18
Sirajudheen Mohamed Ali <sirajudheen.mohamed.ali@sap.com>
2016-12-16 21:10:46 +0100

    fixed config

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ default['mongodb']['edition'] = 'community'
 default['mongodb']['version'] = '3.4'
 
 default['mongodb']['config_file'] = '/etc/mongod.conf'
-default['mongodb']['data_path'] = '/var/lib/mongo'
+default['mongodb']['data_path'] = '/data/db'
 default['mongodb']['pid']['path'] = "/var/run/mongodb"
-default['mongodb']['pid'] = '/var/run/mongodb/mongod.pid'
+default['mongodb']['pid']['file'] = '/var/run/mongodb/mongod.pid'
 default['mongodb']['log_path'] = '/var/log/mongodb/mongod.log'
 default['mongodb']['port'] = '27017'
